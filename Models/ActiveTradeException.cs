9655f0c7708bcee7af5d1e8cd315418ae64c6cd4
Adrian Mueller <adrian.mueller@trumpf.com>
2026-03-25 18:05:03 +0100

    Add Active trade handling and write several todos.

---
+++ Models/ActiveTradeException.cs
@@ -0,0 +1,10 @@
+﻿using DoyVestment.Framework.Models;
+using DoyVestment.Framework.Models.Enums;
+using System.Net;
+
+namespace doylib.Models;
+
+public class ActiveTradeException(
+    string message,
+    HttpStatusCode statusCode,
+    ExceptionSeverityLevel severity) : DoyVestmentException(message, statusCode, severity);
