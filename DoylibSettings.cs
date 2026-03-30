69acec5cb9ac4956905500f164f71263160f2be5
Simone <simone.panico@icloud.com>
2026-03-30 21:17:01 +0200

    Make `Quorum` and `MaxCandleWindowSize` properties immutable in `DoylibSettings`

--- DoylibSettings.cs
+++ DoylibSettings.cs
@@ -1,8 +1,7 @@
 namespace doylib;
 
-// Gets moved to Signalhandlers appsettings.json 
 public class DoylibSettings
 {
-    public double Quorum { get; set; } = 0.5;
-    public int MaxCandleWindowSize { get; set; } = 100;
+    public double Quorum { get; init; }
+    public int MaxCandleWindowSize { get; init; }
 }
